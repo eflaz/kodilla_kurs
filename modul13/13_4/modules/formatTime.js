75572baa5ffd6c5e7e9c8c5c9961c9174049d16b
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-06 12:46:00 +0200

    13.4 improved, 13.8 to review by mentor, 13.9 to review by mentor, 14.2 in process

--- modul13/13_4/modules/formatTime.js
+++ modul13/13_4/modules/formatTime.js
@@ -1,8 +1,13 @@
 function getFormattedTime(seconds) {
-	if (seconds >= 3600) {
-		return Math.floor(seconds / 3600) + ' hours ' + Math.floor((seconds % 3600) / 60) + ' min ' + Math.floor((seconds % 3600) % 60) + ' sec';
-	} else {
-		return Math.floor(seconds / 60) + ' min ' + Math.floor(seconds % 60) + ' sec';
+	var hours = Math.floor(seconds / 3600);
+	var minutes = Math.floor((seconds % 3600) / 60);
+	var restSeconds = Math.floor(seconds % 60);
+	if (hours > 0) {
+		return hours + " hours " + minutes + " minutes " + restSeconds + " seconds";
 	}
+	if (minutes > 0) {
+		return minutes + " minutes " + restSeconds + " seconds";
+	}
+	return restSeconds + " seconds";
 }
 exports.getFormattedTime = getFormattedTime;
