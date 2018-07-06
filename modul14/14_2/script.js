cae718862f53c8fda6602cb6854c81e87ddad9ef
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-07 00:11:39 +0200

    tabs corrected

--- modul14/14_2/script.js
+++ modul14/14_2/script.js
@@ -12,52 +12,38 @@
 //       )
 //     )
 //   );
-
-
-var movies = [
-  {
-  	id: 1,
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju',
-    img: './harry.jpg'
-  },
-  {
-  	id: 2,
-    title: 'Król Lew',
-    desc: 'Film o królu sawanny',
-    img: './lion.jpg'
-  },
-    {
-  	id: 3,
-    title: 'Odlot',
-    desc: 'Bajka o tym, że nigdy nie jest za późno na spełnienie swoich marzeń',
-    img: './odlot.jpg'
-  },
-    {
-  	id: 4,
-    title: 'Lorax',
-    desc: 'Bajka o tym jak zgubny jest konsumpcjonizm i niszczenie przyrody',
-    img: './lorax.jpg'
-  },
-    {
-  	id: 5,
-    title: 'Rio',
-    desc: 'Bajka pełna brazylisjkich rytmów, pięknych widoków i amazońskich zwierząt.',
-    img: './rio.jpg'
-  }
-];
-
+var movies = [{
+  id: 1,
+  title: 'Harry Potter',
+  desc: 'film o czarodzieju',
+  img: './harry.jpg'
+}, {
+  id: 2,
+  title: 'Król Lew',
+  desc: 'Film o królu sawanny',
+  img: './lion.jpg'
+}, {
+  id: 3,
+  title: 'Odlot',
+  desc: 'Bajka o tym, że nigdy nie jest za późno na spełnienie swoich marzeń',
+  img: './odlot.jpg'
+}, {
+  id: 4,
+  title: 'Lorax',
+  desc: 'Bajka o tym jak zgubny jest konsumpcjonizm i niszczenie przyrody',
+  img: './lorax.jpg'
+}, {
+  id: 5,
+  title: 'Rio',
+  desc: 'Bajka pełna brazylisjkich rytmów, pięknych widoków i amazońskich zwierząt.',
+  img: './rio.jpg'
+}];
 var moviesElements = movies.map(function(movie) {
-  return React.createElement('li', {key: movie.id},
-      React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc),
-     React.createElement('img', {src: movie.img})
-    );
+  return React.createElement('li', {
+    key: movie.id
+  }, React.createElement('h2', {}, movie.title), React.createElement('p', {}, movie.desc), React.createElement('img', {
+    src: movie.img
+  }));
 });
-
-var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
-  ReactDOM.render(element, document.getElementById('app'));
+var element = React.createElement('div', {}, React.createElement('h1', {}, 'Lista filmów'), React.createElement('ul', {}, moviesElements));
+ReactDOM.render(element, document.getElementById('app'));
