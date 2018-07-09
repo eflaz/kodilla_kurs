175b0e53a6ead467cd9241fe107807d967b371a2
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-09 22:57:06 +0200

    style.css added

---
+++ modul14/14_6/program.js
@@ -0,0 +1,23 @@
+var Counter = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+	
+	render: function() {
+		return React.createElement('div', {},
+			React.createElement('span', {}, 'Licznik ' + this.state.counter)
+		);
+	}
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+
