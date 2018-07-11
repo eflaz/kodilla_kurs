42529822c26e03673368956171f845cd807bf59b
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-11 21:21:34 +0200

    contact and contactForm JSX used

---
+++ modul14/14_7/components/Contact.js
@@ -0,0 +1,23 @@
+var Contact = React.createClass({
+	propTypes: {
+		item: React.PropTypes.object.isRequired,
+	},
+
+	render: function() {
+		return (
+			<div className={'contactItem'}>
+			<img className={'contactImage'} src={'male-solid.svg'}/>
+			<p className={'contactLabel'}>
+			Imię: {this.props.item.firstName}
+			</p>
+			<p className={'contactLabel'}>
+			Nazwisko: {this.props.item.lastName}
+			</p>
+			<a href={'mailto:' + this.props.item.email}>
+			{this.props.item.email}
+			</a>
+			</div>
+			);
+	},
+});
+
