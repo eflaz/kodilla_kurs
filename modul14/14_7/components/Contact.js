e2a94483a3888572b11fb1f26b29ac8b4612a890
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-13 16:10:34 +0200

    prettier used

--- modul14/14_7/components/Contact.js
+++ modul14/14_7/components/Contact.js
@@ -1,23 +1,16 @@
 var Contact = React.createClass({
-	propTypes: {
-		item: React.PropTypes.object.isRequired,
-	},
+  propTypes: {
+    item: React.PropTypes.object.isRequired
+  },
 
-	render: function() {
-		return (
-			<div className={'contactItem'}>
-			<img className={'contactImage'} src={'male-solid.svg'}/>
-			<p className={'contactLabel'}>
-			Imię: {this.props.item.firstName}
-			</p>
-			<p className={'contactLabel'}>
-			Nazwisko: {this.props.item.lastName}
-			</p>
-			<a href={'mailto:' + this.props.item.email}>
-			{this.props.item.email}
-			</a>
-			</div>
-			);
-	},
+  render: function() {
+    return (
+      <div className={"contactItem"}>
+        <img className={"contactImage"} src={"male-solid.svg"} />
+        <p className={"contactLabel"}>Imię: {this.props.item.firstName}</p>
+        <p className={"contactLabel"}>Nazwisko: {this.props.item.lastName}</p>
+        <a href={"mailto:" + this.props.item.email}>{this.props.item.email}</a>
+      </div>
+    );
+  }
 });
-
