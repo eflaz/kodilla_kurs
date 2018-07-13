f813385b1df060679805d1dc18f25bbc99bce5a1
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-13 16:16:37 +0200

    prettier used

--- modul14/14_8/components/Search.js
+++ modul14/14_8/components/Search.js
@@ -1,39 +1,40 @@
 Search = React.createClass({
-	getInitialState() {
-		return {
-			searchingText: ''
-		};
-	},
-	
-	render: function() {
-		var styles = {
-			fontSize: '1.5em',
-			width: '90%',
-			maxWidth: '350px'
-		};
+  getInitialState() {
+    return {
+      searchingText: ""
+    };
+  },
 
-	return <input
-			type="text"
-			onChange={this.handleChange}
-			placeholder="Tutaj wpisz wyszukiwana frazę"
-			style={styles}
-			value={this.state.searchTerm}
-			/>
-	},
+  render: function() {
+    var styles = {
+      fontSize: "1.5em",
+      width: "90%",
+      maxWidth: "350px"
+    };
 
-	handleChange: function(event) {
-			var searchingText = event.target.value;
-			this.setState({searchingText: searchingText});
+    return (
+      <input
+        type="text"
+        onChange={this.handleChange}
+        placeholder="Tutaj wpisz wyszukiwana frazę"
+        style={styles}
+        value={this.state.searchTerm}
+      />
+    );
+  },
 
-			if (searchingText.length > 2) {
-				this.props.onSearch(searchingText);
-			}
-		},
+  handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({ searchingText: searchingText });
 
-		handleKeyUp: function(event) {
-			if (event.keyCode === 13) {
-				this.props.onSearch(this.state.searchingText);
-			}
-		},
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
 
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  }
 });
