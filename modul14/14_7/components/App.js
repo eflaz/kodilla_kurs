c85909af21fa1720fa9ff7e41589391e6b7d58a7
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-21 22:10:06 +0200

    createElement changed to JSX

--- modul14/14_7/components/App.js
+++ modul14/14_7/components/App.js
@@ -27,11 +27,11 @@ var contactForm = {
 
 var App = React.createClass({
   render: function() {
-    return React.createElement(
-      "div",
-      { className: "app" },
-      React.createElement(ContactForm, { contact: contactForm }),
-      React.createElement(Contacts, { items: contacts }, {})
+    return (
+      <div className='app'>
+        <ContactForm contact = {contactForm}/>
+        <Contacts items = {contacts} /> 
+      </div>
     );
   }
 });
