f40a3092a79e2b0e7a39c3d1f594d96e88843d65
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-27 14:57:16 +0200

    17_3_2 added

---
+++ modul17/17_3_2/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var fs = require("fs");
+var bodyParser = require("body-parser");
+
+var app = express();
+var stringifyFile;
+
+app.use(bodyParser.json());
+
+app.get("/getNote", function(req, res) {
+	console.log("GET request - printing file's content");
+	fs.readFile("./test.json", "utf-8", function(err, data) {
+		if (err) throw err;
+		stringifyFile = data;
+		res.send(data);
+	});
+});
+
+app.post("/updateNote/:note", function(req, res) {
+	console.log("POST request got, adding note to the file...");
+	stringifyFile += req.params.note;
+	fs.writeFile("./test.json", stringifyFile, function(err) {
+		if (err) throw err;
+		console.log("File updated");
+		res.send("file updated");
+	});
+});
+
+app.listen(3000);
+
+app.use(function(req, res, next) {
+	res.status(404).send("404 error");
+});
