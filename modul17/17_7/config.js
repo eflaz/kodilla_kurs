67427c30c7b5a20b39d4e8947f7f5c2918ac795a
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-07-31 23:44:05 +0200

    17_7 added

---
+++ modul17/17_7/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '365246516742-g0j1pvcb4qhi1rf6ofd4r7pat43str8i.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'UsINS2_ehs6iX87-3U8Cfhm6',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
