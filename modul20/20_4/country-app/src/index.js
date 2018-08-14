f4e9ae005350629ebd830b7eea41089cc11920f3
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-08-14 23:15:52 +0200

    bootstrap 3.0 installed, devTool added

--- modul20/20_4/country-app/src/index.js
+++ modul20/20_4/country-app/src/index.js
@@ -5,14 +5,17 @@ import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
 
-// import DevTools from './store/DevTools';
-// import { getCountries } from './actions/actions-countries';
+import DevTools from './store/DevTools';
+import { getCountries } from './actions/actions-countries';
 
-// store.dispatch(getCountries());
+store.dispatch(getCountries());
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes}/>
-        
-    </Provider>,
+        <div>
+            <Router history={hashHistory} routes={routes}>
+            </Router>
+            <DevTools />
+        </div>
+    </Provider >,
     document.getElementById('root')
 );
