049e0a158e433ef1d4349b24e548242e34ea0135
eflaz <ewelinkus@gmail.com>
2018-08-15 20:40:56 +0200

    19.4 - returns corrected

--- modul19/19_4/comments.js
+++ modul19/19_4/comments.js
@@ -1,10 +1,6 @@
-import {ADD_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
+import { ADD_COMMENT, EDIT_COMMENT, REMOVE_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from './actions';
 
-function comments(state =[], action) {
+function comments(state = [], action) {
     switch (action.type) {
         case ADD_COMMENT:
             return [
@@ -16,50 +12,47 @@ function comments(state =[], action) {
                 , ...state.comments
             ]
         case EDIT_COMMENT:
-        return state.comments.map((comment) => {
-            if (comment.id !== action.id) {
-                return comment;
-            } else {
-                return {
-                    id: comment.id,
-                    text: action.text,
-                    votes: comment.votes
+            return state.comments.map((comment) => {
+                if (comment.id !== action.id) {
+                    return comment;
+                } else {
+                    return {
+                        ...comment,
+                        text: action.text
+                    }
                 }
-            }
-        });
+            });
 
         case REMOVE_COMMENT:
             return state.comments.filter(comment => comment.id !== action.id)
 
         case THUMB_UP_COMMENT:
-        return state.comments.map((comment) => {
-            if (comment.id !== action.id) {
-                return comment;
-            } else {
-                return {
-                    id: comment.id,
-                    text: action.text,
-                    votes: comment.votes +1
+            return state.comments.map((comment) => {
+                if (comment.id !== action.id) {
+                    return comment;
+                } else {
+                    return {
+                        ...comment,
+                        votes: comment.votes + 1
+                    }
                 }
-            }
-        });
+            });
 
         case THUMB_DOWN_COMMENT:
-        return state.comments.map((comment) => {
-            if (comment.id !== action.id) {
-                return comment;
-            } else {
-                return {
-                    id: comment.id,
-                    text: action.text,
-                    votes: comment.votes -1
+            return state.comments.map((comment) => {
+                if (comment.id !== action.id) {
+                    return comment;
+                } else {
+                    return {
+                        ...comment,
+                        votes: comment.votes - 1
+                    }
                 }
-            }
-        });
+            });
 
         default:
-        return state;
-   
+            return state;
+
     }
 }
 
