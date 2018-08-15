219743695f4beb4aef04013aa952d5c1b171ccd7
eflaz <ewelinkus@gmail.com>
2018-08-15 20:34:49 +0200

    20.5 - view corrected

--- modul20/20_5/country-app/src/index.js
+++ modul20/20_5/country-app/src/index.js
@@ -7,15 +7,16 @@ import routes from './routes';
 
 import DevTools from './store/DevTools';
 import { getCountries } from './actions/actions-countries';
+import './country.css';
 
 store.dispatch(getCountries());
 render(
     <Provider store={store}>
-    <div>
-        <Router history={hashHistory} routes={routes}>
-        </Router>
-        <DevTools />
-    </div>
-</Provider >,
+        <div>
+            <Router history={hashHistory} routes={routes}>
+            </Router>
+            <DevTools />
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
