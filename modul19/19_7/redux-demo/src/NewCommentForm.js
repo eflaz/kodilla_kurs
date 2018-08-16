f1d565c8c732a2e1dff40154e65ac02e93470aaa
eflaz <ewelinkus@gmail.com>
2018-08-16 15:54:22 +0200

    19.7 - JSX in NewCommentForm corrected

--- modul19/19_7/redux-demo/src/NewCommentForm.js
+++ modul19/19_7/redux-demo/src/NewCommentForm.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
+const onSubmit = addComment => event => {
+    event.preventDefault();
+    addComment(event.target['newCommentText'].value);
+    event.target['newCommentText'].value = '';
+}
+
 const NewCommentForm = ({addComment}) =>
-<div>
-    <form onSubmit={
-        (event) => {
-            event.preventDefault();
-            addComment(event.target['newCommentText'].value);
-            event.target['newCommentText'].value = '';
-        }
-    }
- >
- <input type="text" name="newCommentText" />
- </form>
- </div>
+    <div>
+        <form onSubmit={onSubmit(addComment)}>
+            <input type="text" name="newCommentText" />
+        </form>
+    </div>
 
  export default NewCommentForm;
